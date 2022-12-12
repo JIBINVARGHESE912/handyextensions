11016b4a9b3b088995168d70b1b843f73ac4d0bb
JIBIN VARGHESE <jibin.varghese.ext@bayer.com>
2022-12-12 14:16:09 +0530

    Initial commit

---
+++ Example/HandyExtensions/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HandyExtensions
+//
+//  Created by JIBIN VARGHESE on 12/12/2022.
+//  Copyright (c) 2022 JIBIN VARGHESE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
